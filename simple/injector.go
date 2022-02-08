8337a25c8ce4832969fa9cd3f85586751616b97e
raviMukti <shinjiravi@gmail.com>
2022-02-08 17:06:40 +0700

    Grouping or Provider Set

--- simple/injector.go
+++ simple/injector.go
@@ -18,3 +18,11 @@ func InitializeDatabaseRepository() *DatabaseRepository {
 	)
 	return nil
 }
+
+var fooSet = wire.NewSet(NewFooRepository, NewFooService)
+var barSet = wire.NewSet(NewBarRepository, NewBarService)
+
+func InitializedFooBarService() *FooBarService {
+	wire.Build(fooSet, barSet, NewFooBarService)
+	return nil
+}
